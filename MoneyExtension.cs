be8b023cc65fdca71da3410d22d5fa81f3d3c81e
Matheus Henrique <seuemail@exemplo.com>
2024-12-24 09:02:12 -0300

    Initial commit

---
+++ MoneyExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace MoneyExtension;
+
+public static class Money
+{
+    public static int ToCents(this decimal amount)
+    {
+        if (amount < 0)
+            return 0;
+
+        // Converte o valor decimal para string com duas casas decimais
+        var text = amount.ToString("F2").Replace(",", "").Replace(".", "");
+
+        if (string.IsNullOrEmpty(text))
+            return 0;
+
+        // Tenta converter a string para inteiro
+        if (int.TryParse(text, out var result))
+            return result;
+
+        return 0; // Retorna 0 caso a conversão falhe
+    }
+}
